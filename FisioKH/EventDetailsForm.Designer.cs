210f745ebd2927bdeb6c52745b992f3ab4269d06
Gabriel Vizcarra <gvizcarra@gmail.com>
2026-01-14 14:45:58 -0800

    ok con calendar panel

---
+++ FisioKH/EventDetailsForm.Designer.cs
@@ -0,0 +1,164 @@
+﻿namespace FisioKH
+{
+    partial class EventDetailsForm
+    {
+        private System.ComponentModel.IContainer components = null;
+
+        private System.Windows.Forms.Label lblTitle;
+        private System.Windows.Forms.TextBox txtTitle;
+        private System.Windows.Forms.Label lblStart;
+        private System.Windows.Forms.DateTimePicker dtStart;
+        private System.Windows.Forms.Label lblEnd;
+        private System.Windows.Forms.DateTimePicker dtEnd;
+        private System.Windows.Forms.Label lblColor;
+        private System.Windows.Forms.Panel pnlColor;
+        private System.Windows.Forms.Button btnColor;
+        private System.Windows.Forms.Button btnSave;
+        private System.Windows.Forms.Button btnDelete;
+        private System.Windows.Forms.ColorDialog colorDialog1;
+
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+                components.Dispose();
+
+            base.Dispose(disposing);
+        }
+
+        private void InitializeComponent()
+        {
+            this.lblTitle = new System.Windows.Forms.Label();
+            this.txtTitle = new System.Windows.Forms.TextBox();
+            this.lblStart = new System.Windows.Forms.Label();
+            this.dtStart = new System.Windows.Forms.DateTimePicker();
+            this.lblEnd = new System.Windows.Forms.Label();
+            this.dtEnd = new System.Windows.Forms.DateTimePicker();
+            this.lblColor = new System.Windows.Forms.Label();
+            this.pnlColor = new System.Windows.Forms.Panel();
+            this.btnColor = new System.Windows.Forms.Button();
+            this.btnSave = new System.Windows.Forms.Button();
+            this.btnDelete = new System.Windows.Forms.Button();
+            this.colorDialog1 = new System.Windows.Forms.ColorDialog();
+            this.SuspendLayout();
+            // 
+            // lblTitle
+            // 
+            this.lblTitle.AutoSize = true;
+            this.lblTitle.Location = new System.Drawing.Point(12, 15);
+            this.lblTitle.Name = "lblTitle";
+            this.lblTitle.Size = new System.Drawing.Size(27, 13);
+            this.lblTitle.TabIndex = 0;
+            this.lblTitle.Text = "Title";
+            // 
+            // txtTitle
+            // 
+            this.txtTitle.Location = new System.Drawing.Point(60, 12);
+            this.txtTitle.Name = "txtTitle";
+            this.txtTitle.Size = new System.Drawing.Size(200, 20);
+            this.txtTitle.TabIndex = 1;
+            // 
+            // lblStart
+            // 
+            this.lblStart.AutoSize = true;
+            this.lblStart.Location = new System.Drawing.Point(12, 48);
+            this.lblStart.Name = "lblStart";
+            this.lblStart.Size = new System.Drawing.Size(29, 13);
+            this.lblStart.TabIndex = 2;
+            this.lblStart.Text = "Start";
+            // 
+            // dtStart
+            // 
+            this.dtStart.CustomFormat = "yyyy-MM-dd HH:mm";
+            this.dtStart.Format = System.Windows.Forms.DateTimePickerFormat.Custom;
+            this.dtStart.Location = new System.Drawing.Point(60, 45);
+            this.dtStart.Name = "dtStart";
+            this.dtStart.Size = new System.Drawing.Size(200, 20);
+            this.dtStart.TabIndex = 3;
+            // 
+            // lblEnd
+            // 
+            this.lblEnd.AutoSize = true;
+            this.lblEnd.Location = new System.Drawing.Point(12, 82);
+            this.lblEnd.Name = "lblEnd";
+            this.lblEnd.Size = new System.Drawing.Size(26, 13);
+            this.lblEnd.TabIndex = 4;
+            this.lblEnd.Text = "End";
+            // 
+            // dtEnd
+            // 
+            this.dtEnd.CustomFormat = "yyyy-MM-dd HH:mm";
+            this.dtEnd.Format = System.Windows.Forms.DateTimePickerFormat.Custom;
+            this.dtEnd.Location = new System.Drawing.Point(60, 79);
+            this.dtEnd.Name = "dtEnd";
+            this.dtEnd.Size = new System.Drawing.Size(200, 20);
+            this.dtEnd.TabIndex = 5;
+            // 
+            // lblColor
+            // 
+            this.lblColor.AutoSize = true;
+            this.lblColor.Location = new System.Drawing.Point(12, 116);
+            this.lblColor.Name = "lblColor";
+            this.lblColor.Size = new System.Drawing.Size(31, 13);
+            this.lblColor.TabIndex = 6;
+            this.lblColor.Text = "Color";
+            // 
+            // pnlColor
+            // 
+            this.pnlColor.BorderStyle = System.Windows.Forms.BorderStyle.FixedSingle;
+            this.pnlColor.Location = new System.Drawing.Point(60, 113);
+            this.pnlColor.Name = "pnlColor";
+            this.pnlColor.Size = new System.Drawing.Size(40, 20);
+            this.pnlColor.TabIndex = 7;
+            // 
+            // btnColor
+            // 
+            this.btnColor.Location = new System.Drawing.Point(110, 111);
+            this.btnColor.Name = "btnColor";
+            this.btnColor.Size = new System.Drawing.Size(30, 23);
+            this.btnColor.TabIndex = 8;
+            this.btnColor.Text = "...";
+            this.btnColor.Click += new System.EventHandler(this.btnColor_Click);
+            // 
+            // btnSave
+            // 
+            this.btnSave.Location = new System.Drawing.Point(40, 150);
+            this.btnSave.Name = "btnSave";
+            this.btnSave.Size = new System.Drawing.Size(80, 25);
+            this.btnSave.TabIndex = 9;
+            this.btnSave.Text = "Save";
+            this.btnSave.Click += new System.EventHandler(this.btnSave_Click);
+            // 
+            // btnDelete
+            // 
+            this.btnDelete.Location = new System.Drawing.Point(150, 150);
+            this.btnDelete.Name = "btnDelete";
+            this.btnDelete.Size = new System.Drawing.Size(80, 25);
+            this.btnDelete.TabIndex = 10;
+            this.btnDelete.Text = "Delete";
+            this.btnDelete.Click += new System.EventHandler(this.btnDelete_Click);
+            // 
+            // EventDetailsForm
+            // 
+            this.ClientSize = new System.Drawing.Size(333, 225);
+            this.Controls.Add(this.lblTitle);
+            this.Controls.Add(this.txtTitle);
+            this.Controls.Add(this.lblStart);
+            this.Controls.Add(this.dtStart);
+            this.Controls.Add(this.lblEnd);
+            this.Controls.Add(this.dtEnd);
+            this.Controls.Add(this.lblColor);
+            this.Controls.Add(this.pnlColor);
+            this.Controls.Add(this.btnColor);
+            this.Controls.Add(this.btnSave);
+            this.Controls.Add(this.btnDelete);
+            this.FormBorderStyle = System.Windows.Forms.FormBorderStyle.FixedDialog;
+            this.MaximizeBox = false;
+            this.Name = "EventDetailsForm";
+            this.StartPosition = System.Windows.Forms.FormStartPosition.CenterParent;
+            this.Text = "Event Details";
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+    }
+}
---
+++ FisioKH/EventDetailsForm.cs
@@ -0,0 +1,53 @@
+﻿using System;
+using System.Drawing;
+using System.Windows.Forms;
+
+namespace FisioKH
+{
+    public partial class EventDetailsForm : Form
+    {
+        public CalendarEventx Event { get; private set; }
+        public bool Deleted { get; private set; }
+
+        // REQUIRED for Designer
+        public EventDetailsForm()
+        {
+            InitializeComponent();
+        }
+
+        // Runtime constructor
+        public EventDetailsForm(CalendarEventx ev) : this()
+        {
+            Event = ev;
+
+            txtTitle.Text = ev.Title;
+            dtStart.Value = ev.StartTime;
+            dtEnd.Value = ev.EndTime;
+            pnlColor.BackColor = ev.Color;
+        }
+
+        private void btnSave_Click(object sender, EventArgs e)
+        {
+            Event.Title = txtTitle.Text;
+            Event.StartTime = dtStart.Value;
+            Event.EndTime = dtEnd.Value;
+            Event.Color = pnlColor.BackColor;
+
+            DialogResult = DialogResult.OK;
+        }
+
+        private void btnDelete_Click(object sender, EventArgs e)
+        {
+            Deleted = true;
+            DialogResult = DialogResult.OK;
+        }
+
+        private void btnColor_Click(object sender, EventArgs e)
+        {
+            if (colorDialog1.ShowDialog() == DialogResult.OK)
+            {
+                pnlColor.BackColor = colorDialog1.Color;
+            }
+        }
+    }
+}
